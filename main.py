3fbe0e70fa0af156330a247587def17a98aadf3b
Nguyễn Trí Hải <155034181+banhsukemm@users.noreply.github.com>
2024-12-12 22:24:39 +0700

    Update main.py - update for case study

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 import time
 import gym_cutting_stock
 import gymnasium as gym
-from policy import GreedyPolicy2, RandomPolicy
-from student_submissions.s2210xxx.policy2210xxx import Policy2314047, Policy2210xxx
+from policy import GreedyPolicy2, RandomPolicy2
+from student_submissions.s2210xxx.policy2210xxx import Policy2314047
 # Create the environment
 env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 100
-
 if __name__ == "__main__":
-    observation, info = env.reset(seed=40)
-    print(info)
+    observation, info = env.reset(seed=2)
 
     policy2210xxx = Policy2314047()
-    # for _ in range(100):
-    #     action = policy2210xxx.get_action(observation, info)
-    #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
-
-    #     if terminated or truncated:
-    #         observation, info = env.reset()
-    for __ in range(3):
-        demands = 0
-        for prod in observation["products"]:
-            demands += prod["quantity"]
-        print("Demands: ", demands)
-        print(observation)
-        for _ in range(demands):
+    for __ in range(1): # run only 1 env for comparing 2 Policy
+        start_time = time.time()
+        curren_action = {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}
+        current_info = info
+        stock_idx = -1
+        action = {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}
+        while True:
+            last_action = action
             action = policy2210xxx.get_action(observation, info)
             observation, reward, terminated, truncated, info = env.step(action)
-            print(info)
-
+            if (info["filled_ratio"] - current_info["filled_ratio"] > 0):
+                if stock_idx > -1:
+                    print("Used stock index:",last_action["stock_idx"],f",trim loss: {current_info["trim_loss"]:.4f}")
+                stock_idx += 1
+            current_info = info
             if terminated or truncated:
+                print("Used stock index:",action["stock_idx"],f",trim loss: {current_info["trim_loss"]:.4f}")
+                elapsed_time = time.time() - start_time
+                print(f"Elapsed time: {elapsed_time:.4f} seconds")
+                print("Total stock used:", stock_idx + 1)
                 observation, info = env.reset()
                 break
 
